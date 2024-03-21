466fd8a427a099a7bac639ebf02cefcff6b409aa
Saubhagya <saubhagyasingh65@gmail.com>
2024-03-21 06:05:11 +0530

    pages added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
-import { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar.jsx";
 import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Play from "./pages/Play.jsx";
+import Train from "./pages/Train.jsx";
+import MyBots from "./pages/MyBots.jsx";
 function App() {
   return (
     <div>
@@ -10,6 +14,10 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/train" element={<Train />} />
+          <Route exact path="/myBots" element={<MyBots />} />
+          <Route exact path="/play_the_dev" element={<Play />} />
+          <Route exact path="/about" element={<About />} />
         </Routes>
       </Router>
     </div>
